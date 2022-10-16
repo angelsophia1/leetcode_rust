bab51c47938fc5f4d4c631bd06177c24d8212624
angelsophia1 <angelsunyulei@126.com>
2022-10-16 12:43:52 -0700

    Added leetcode_2441 and unit tests

---
+++ leetcode_rust/src/leetcode_2441.rs
@@ -0,0 +1,17 @@
+use std::collections::HashSet;
+#[allow(dead_code)]
+pub struct Solution;
+#[allow(dead_code)]
+impl Solution {
+    pub fn find_max_k(nums: Vec<i32>) -> i32 {
+        let mut nums_set = HashSet::new();
+        let mut max_k = -1;
+        for num in nums.into_iter() {
+            if nums_set.contains(&-num) {
+                max_k = max_k.max(num.abs());
+            }
+            nums_set.insert(num);
+        }
+        max_k
+    }
+}

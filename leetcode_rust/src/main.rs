d82a93edde08f85e4d4eb76e3974c72f7941eba3
sophia-sun-hamster <angelsunyulei@gmail.com>
2022-10-16 12:01:28 -0700

    Create directory leetcode_rust and two_sum.rs

---
+++ leetcode_rust/src/main.rs
@@ -0,0 +1,17 @@
+use crate::two_sum::Solution;
+mod two_sum;
+fn main() {
+    println!("Hello World");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn test_two_sum() {
+        let mut two_sum_res = Solution::two_sum(vec![2,7,11,15], 9);
+        two_sum_res.sort();
+        assert_eq!(vec![0, 1], two_sum_res);
+    }
+}
